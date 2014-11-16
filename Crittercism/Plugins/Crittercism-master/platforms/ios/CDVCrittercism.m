12b70fd8d57b5ba0e80e07fade0565b21a4a5fb8
Eddy Verbruggen <eddyverbruggen@gmail.com>
2014-11-16 10:52:03 +0000

    plugin updated to 2.0.0

---
+++ Crittercism/Plugins/Crittercism-master/platforms/ios/CDVCrittercism.m
@@ -0,0 +1,132 @@
+//
+//  CDVCrittercism.m
+//  Crittercism PhoneGap
+//
+//  Created by Crittercism on 7/6/14.
+//  Copyright (c) 2014 Crittercism. All rights reserved.
+
+#import "CDVCrittercism.h"
+#import "Crittercism.h"
+
+void Crittercism_LogHandledException(const char* name,
+                                     const char* reason,
+                                     const char *stack,
+                                     int platformId);
+
+void Crittercism_LogUnhandledException(const char* name,
+                                       const char* reason,
+                                       const char *stack,
+                                       int platformId);
+
+static NSString *const CRJavascriptXMLHttpRequest = @"JavascriptXMLHttpRequest";
+
+@implementation CDVCrittercism
+
+- (void)pluginInitialize
+{
+  NSString *CritterAppID = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CritterAppID"];
+  NSLog(@"Initializing Crittercism for application with app id %@", CritterAppID);
+  [Crittercism enableWithAppID:CritterAppID];
+}
+
+- (void) crittercismLeaveBreadcrumb:(CDVInvokedUrlCommand *)command {
+  [self.commandDelegate runInBackground:^{
+    NSString* breadcrumb = command.arguments[0];
+    [Crittercism leaveBreadcrumb:breadcrumb];
+  }];
+}
+
+- (void) crittercismLogHandledException:(CDVInvokedUrlCommand *)command {
+  [self.commandDelegate runInBackground:^{
+    NSString* name = command.arguments[0];
+    NSString* message = command.arguments[1];
+    NSString* stack = command.arguments[2];
+    stack = [self deleteStackFillerInformation:stack];
+
+    const char *cName= [name UTF8String];
+    const char *cMessage= [message UTF8String];
+    const char *cStack= [stack UTF8String];
+
+    Crittercism_LogHandledException(cName, cMessage, cStack, 2);
+  }];
+}
+
+- (void) crittercismLogUnhandledException:(CDVInvokedUrlCommand *)command {
+  [self.commandDelegate runInBackground:^{
+    NSString* message = command.arguments[0];
+    NSString* stack = command.arguments[1];
+    stack = [self deleteStackFillerInformation:stack];
+
+    const char *cName = "Error";
+    const char *cMessage= [message UTF8String];
+    const char *cStack= [stack UTF8String];
+
+    Crittercism_LogUnhandledException(cName, cMessage, cStack, 2);
+  }];
+}
+
+- (void) crittercismSetUsername:(CDVInvokedUrlCommand *)command {
+  [self.commandDelegate runInBackground:^{
+    NSString* username = command.arguments[0];
+    [Crittercism setUsername:username];
+  }];
+}
+
+- (void) crittercismSetValueForKey:(CDVInvokedUrlCommand *)command {
+  [self.commandDelegate runInBackground:^{
+    NSString* key = command.arguments[0];
+    NSString* value = command.arguments[1];
+    [Crittercism setValue:value
+                   forKey:key];
+  }];
+}
+
+- (void) crittercismLogNetworkRequest:(CDVInvokedUrlCommand *)command {
+  [self.commandDelegate runInBackground:^{
+    NSString* method = command.arguments[0];
+    NSURL* url = [NSURL URLWithString:command.arguments[1]];
+    NSUInteger latencyMillis = [command.arguments[2] unsignedLongValue];
+    NSTimeInterval latency = latencyMillis / 1000.0;
+    NSUInteger bytesRead = [command.arguments[3] unsignedIntegerValue];
+    NSUInteger bytesSent = [command.arguments[4] unsignedIntegerValue];
+    NSInteger responseCode = [command.arguments[5] integerValue];
+    NSError *error;
+
+	@try {
+        NSInteger errorInt = [command.arguments[6] integerValue];
+        error = [[NSError alloc] initWithDomain:CRJavascriptXMLHttpRequest
+                                               code:errorInt
+                                           userInfo:nil];
+	} @catch (NSException *exception) {
+        error = nil;
+	}
+
+    [Crittercism logNetworkRequest:method
+                               url:url
+                           latency:latency
+                         bytesRead:bytesRead
+                         bytesSent:bytesSent
+                      responseCode:responseCode
+                             error:error];
+  }];
+}
+
+// Examples below
+//
+// FunctionC@file:///Users/tshi/Library/Application%20Support/iPhone%20Simulator/6.1/Applications/29A502DF-F664-434A-94C6-12AAA20BCF33/HelloWorld.app/www/js/app.js:30
+// -> FunctionC@HelloWorld.app/www/js/app.js:30
+//
+// {anonymous}("Error: Error!","file:///Users/tshi/Library/Application%20Support/iPhone%20Simulator/6.1/Applications/29A502DF-F664-434A-94C6-12AAA20BCF33/HelloWorld.app/www/js/app.js",22)
+// -> {anonymous}("Error: Error!","js/app.js",22)
+
+- (NSString*) deleteStackFillerInformation:(NSString *) stack {
+  NSString* appName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
+  NSString* pattern = [NSString stringWithFormat:@"file:\/\/.*(?=%@)", appName];
+  NSError* error = NULL;
+  NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:pattern
+                                                                         options:NSRegularExpressionCaseInsensitive
+                                                                           error:&error];
+  return [regex stringByReplacingMatchesInString:stack options:0 range:NSMakeRange(0, [stack length]) withTemplate:@""];
+}
+
+@end
--- Crittercism/js/app.js
+++ Crittercism/js/app.js
@@ -17,21 +17,21 @@
  * under the License.
  */
 
- var error1 = function ThrowNewError() {
-   return function FunctionB() {
-     return function FunctionC() {
-       throw new Error( "Error!" );
-     }
-   }
- }
+var error1 = function ThrowNewError() {
+    return function FunctionB() {
+        return function FunctionC() {
+            throw new Error( "Error!" );
+        }
+    }
+};
 
- var error2 = function NonsenseText() {
-   return function FunctionB() {
-     return function FunctionC() {
-       abc123       // ridiculous nonsense that causes a crash
-     }
-   }
- }
+var error2 = function NonsenseText() {
+    return function FunctionB() {
+        return function FunctionC() {
+            abc123       // ridiculous nonsense that causes a crash
+        }
+    }
+};
 
 var app = {
     // Application Constructor
@@ -50,13 +50,50 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        $('#crashButton').click(function() {
+        var url_scheme = "http",
+          sync = false,
+          networking_api = "xhr",
+          response_code,
+          data,
+          delay,
+          jqXHR,
+          XHR,
+          timeout = 0,
+          http_method = "GET",
+          bytes_out = 0,
+          bytes_in = 0
+          make_request = makeXMLHttpRequest;
+
+        function makeXMLHttpRequest(URL) {
+            XHR = new window.XMLHttpRequest();
+            XHR.timeout = timeout;
+            XHR.open(http_method, URL, !sync);
+            XHR.onloadend = function() {
+                alert("Received response(" + XHR.status + ") from " + URL);
+            };
+            console.log("sending data: " + data);
+            XHR.send(data);
+        }
+
+        function makeJQRequest(URL) {
+            jqXHR = $.ajax({
+                url: URL,
+                type: http_method,
+                async: !sync,
+                timeout: timeout,
+                data: data,
+                complete: function() {
+                    alert("Received response from " + URL + "\nBody:\n" + this.response);
+                }
+            });
+        }
+
+        $('#crash_application').click(function() {
             error1()()();       // ridiculous nonsense that causes a crash
         });
 
         $('#breadcrumb_submit').click(function() {
-            var bcText = 'breadcrumbb';
-            Crittercism.leaveBreadcrumb(bcText);
+            Crittercism.leaveBreadcrumb('breadcrumb');
         });
 
         $('#set_username').click(function() {
@@ -69,11 +106,110 @@ var app = {
 
         $('#handled_exception').click(function() {
             try {
-              error2()()();
+                error2()()();
             } catch(e) {
                 Crittercism.logHandledException(e);
             }
         });
+
+        $('#test_APM').click(function() {
+            $('#default').fadeOut();
+            $('#service_monitoring').fadeIn();
+        });
+
+        $("input:radio[name=url_scheme]").click(function() {
+            url_scheme = $('input:radio[name=url_scheme]:checked').val();
+            console.log("set base url to " + url_scheme);
+        });
+
+        $("input:radio[name=sync]").click(function() {
+            var bool = $('input:radio[name=sync]:checked').val();
+            sync = (bool === "true");
+            console.log("set synchronous to " + sync);
+        });
+
+        $("input:radio[name=networking_api]").click(function() {
+            networking_api = $('input:radio[name=networking_api]:checked').val();
+
+            if (networking_api) {
+              make_request = makeJQRequest;
+            } else {
+              make_request = makeXMLHttpRequest;
+            }
+
+            console.log("set networking_api to " + networking_api);
+        });
+
+        $('#response_code_202').click(function() {
+            make_request(url_scheme + "://httpbin.org/status/202");
+        });
+
+        $('#response_code_404').click(function() {
+            make_request(url_scheme + "://httpbin.org/status/404");
+        });
+
+        $('#response_code_500').click(function() {
+            make_request(url_scheme + "://httpbin.org/status/500");
+        });
+
+        $('#delay_1').click(function() {
+            make_request(url_scheme + "://httpbin.org/delay/1");
+        });
+
+        $('#delay_2').click(function() {
+            make_request(url_scheme + "://httpbin.org/delay/2");
+        });
+
+        $('#delay_5').click(function() {
+            make_request(url_scheme + "://httpbin.org/delay/5");
+        });
+
+        $("input:radio[name=timeout]").click(function() {
+            timeout = parseInt($('input:radio[name=timeout]:checked').val());
+            console.log("set timeout to " + timeout + " ms");
+        });
+
+        $("input:radio[name=http_method]").click(function() {
+            http_method = $('input:radio[name=http_method]:checked').val();
+            console.log("set http method to " + http_method);
+        });
+
+        $("input:radio[name=bytes_out]").click(function() {
+            bytes_out = parseInt($('input:radio[name=bytes_out]:checked').val());
+            data = undefined;
+
+            if(bytes_out > 0){
+                data = new Array(bytes_out + 1).join( 'a' );
+            }
+
+        });
+
+        $('#bytes_in_0').click(function() {
+            make_request(url_scheme + "://httpbin.org/bytes/0");
+        });
+
+        $('#bytes_in_4k').click(function() {
+            make_request(url_scheme + "://httpbin.org/bytes/4096");
+        });
+
+        $('#bytes_in_8k').click(function() {
+            make_request(url_scheme + "://httpbin.org/bytes/8192");
+        });
+
+        $('#go_back').click(function() {
+            $('#service_monitoring').fadeOut();
+            $('#default').fadeIn();
+        });
+
+        $('abort_request').click(function() {
+            if(networking_api === "jquery") {
+                jqXHR.abort();
+            } else {
+                XHR.abort();
+            }
+        });
+
+        $('#service_monitoring').hide();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
